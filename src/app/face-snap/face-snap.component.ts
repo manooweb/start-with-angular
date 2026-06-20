5216cf65705bd42bb06813416f665e0af6ddfac9
Emmanuel Hesry <ehesry@gmail.com>
2026-06-20 16:27:20 +0200

    First event binding with a simple click event on a button

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -13,7 +13,8 @@ export class FaceSnapComponent implements OnInit {
   createdAt!: Date;
   snaps!: number;
   imageUrl!: string;
-
+  snapped!: boolean;
+  buttonLabel!: string;
 
   ngOnInit(): void {
     this.title = 'Mon premier snap';
@@ -21,6 +22,19 @@ export class FaceSnapComponent implements OnInit {
     this.createdAt = new Date();
     this.snaps = 0;
     this.imageUrl = 'https://media.istockphoto.com/id/909772496/fr/photo/ours-en-peluche-brun-isol%C3%A9-devant-un-fond-blanc.jpg?s=612x612&w=0&k=20&c=0ONAqN7He2ac1a3iyzlMcKIJnI8dtKiRsbeeecMm1d8=';
+    this.snapped = false;
+    this.buttonLabel = 'Oh Snap!';
   }
 
+  onAddSnap() {
+    if (!this.snapped) {
+      this.snaps++;
+      this.snapped = true;
+      this.buttonLabel = 'Oops, unSnap!';
+    } else {
+      this.snaps--;
+      this.snapped = false;
+      this.buttonLabel = 'Oh Snap!';
+    }
+  }
 }
