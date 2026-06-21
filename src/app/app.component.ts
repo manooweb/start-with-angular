6404cc91f903986fc5817ba547b1d1fcb8ddf4b2
Emmanuel Hesry <ehesry@gmail.com>
2026-06-21 11:05:07 +0200

    Use control flow block @for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,34 +10,32 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  faceSnap!: FaceSnap;
-  secondFaceSnap!: FaceSnap;
-  thirdFaceSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.faceSnap = new FaceSnap(
-      'Mon premier snap',
-      'Mon premier snap description',
-      'https://cdn.pixabay.com/photo/2019/05/27/00/01/teddy-bear-4231582_1280.jpg',
-      new Date(),
-      0
-    );
-    this.faceSnap.setLocation('At home');
-
-    this.secondFaceSnap = new FaceSnap(
-      'Mon deuxième snap',
-      'Mon deuxième snap description',
-      'https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954_1280.jpg',
-      new Date(),
-      0
-    );
-
-    this.thirdFaceSnap = new FaceSnap(
-      'Mon troisième snap',
-      'Mon troisième snap description',
-      'https://cdn.pixabay.com/photo/2022/11/27/13/22/tree-7619791_1280.jpg',
-      new Date(),
-      0
-    );
+    this.faceSnaps = [
+      new FaceSnap(
+        'Mon premier snap',
+        'Mon premier snap description',
+        'https://cdn.pixabay.com/photo/2019/05/27/00/01/teddy-bear-4231582_1280.jpg',
+        new Date(),
+        0
+      ),
+      new FaceSnap(
+        'Mon deuxième snap',
+        'Mon deuxième snap description',
+        'https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954_1280.jpg',
+        new Date(),
+        0
+      ),
+      new FaceSnap(
+        'Mon troisième snap',
+        'Mon troisième snap description',
+        'https://cdn.pixabay.com/photo/2022/11/27/13/22/tree-7619791_1280.jpg',
+        new Date(),
+        0
+      )
+    ];
+    this.faceSnaps[0].setLocation('At home');
   }
 }
